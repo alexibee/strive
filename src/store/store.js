8dbdfd92b0100fd7680f9b47069aaee5d3292cb5
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-11-05 12:01:46 +0000

    persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,18 +1,31 @@
 import logger from 'redux-logger';
 import { configureStore } from '@reduxjs/toolkit';
 import { userReducer } from './user/user.reducer';
+import { combineReducers } from 'redux';
 import { categoriesReducer } from './categories/category.reducer';
 import { cartReducer } from './cart/cart.reducer';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-const rootReducer = {
+const rootReducer = combineReducers({
 	user: userReducer,
 	categories: categoriesReducer,
 	cart: cartReducer,
+});
+
+const persistConfig = {
+	key: 'root',
+	storage,
+	blacklist: ['user'],
 };
 
-const middlewares = [logger];
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middlewares = [process.env.NODE_ENV === 'development' && logger];
 
 export const store = configureStore({
-	reducer: rootReducer,
+	reducer: persistedReducer,
 	middleware: middlewares,
 });
+
+export const persistor = persistStore(store);
